f112bd3603edaffe5aaa25818226738238db7d62
mjanvi17 <janvimistry174@gmail.com>
2025-03-03 11:14:40 +0530

    route

--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth-user/login/login.component';
+import { UserComponent } from './auth-user/user/user.component';
+import { RegisterComponent } from './auth-user/register/register.component';
+import { ForgorpComponent } from './auth-user/forgorp/forgorp.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component :LoginComponent
+    },
+    {
+        path:'user',
+        component:UserComponent
+    },
+    {
+        path:'register',
+        component:RegisterComponent
+    },
+    {
+        path:'forgorp',
+        component:ForgorpComponent
+    }
+];
