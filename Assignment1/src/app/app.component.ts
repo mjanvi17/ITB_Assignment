38d7a10f873cc5a73f2ff76d2b99b472d4a6458b
mjanvi17 <janvimistry174@gmail.com>
2025-03-03 21:55:22 +0530

    routing

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './auth-user/login/login.component';
 import { RegisterComponent } from './auth-user/register/register.component';
 import { ForgorpComponent } from './auth-user/forgorp/forgorp.component';
 import { UserComponent } from './auth-user/user/user.component';
 @Component({
   selector: 'app-root',
-  imports: [UserComponent,RouterOutlet,RouterLink],
+  imports: [UserComponent,RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
